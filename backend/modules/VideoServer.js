1c293d691f813f6035b481e41d1d976aa8509986
Jens Himmelrath <jens.himmelrath@sap.com>
2015-08-12 21:10:21 +0200

    Backend Tag support

--- backend/modules/VideoServer.js
+++ backend/modules/VideoServer.js
@@ -44,80 +44,6 @@ function VideoServer(appDirectory, dataDirectory) {
 
 }
 
-VideoServer.prototype.refresh = function() {
-	this._readInfoIndex();
-};
-
-VideoServer.prototype.infoDefaults = function(info) {
-	if (info === undefined) {
-		// Getter
-		return this._infoDefault;
-	} else {
-		// Setter
-		this._infoDefault = info;
-		return this;
-	}
-};
-
-VideoServer.prototype._readInfoIndex = function() {
-	var indexFile = this._path.join(this._dir, ".mediaIndex.json");
-	try {
-		this._infoIndex = JSON.parse(this._fs.readFileSync(indexFile, { encoding: "utf8" }));
-	} catch (error) {
-		if (error.code === "ENOENT") {
-			this._fs.writeFileSync(indexFile, "{}", { encoding: "utf8" });
-		} else {
-			console.error("Error reading index: " + JSON.stringify(error));
-		}
-		this._infoIndex = {};
-	}
-};
-
-VideoServer.prototype._updateInfoIndex = function() {
-	return new Promise(function(resolve, reject) {
-		var directory = this._dir;
-		this._fs.readdir(directory, function(error, files) {
-			if (error) {
-				reject(error);
-			} else {
-				var filter = this.filter();
-				if (filter && filter.extensions) {
-					files = files.filter(function(file) {
-						var ext = this._path.extname(file).toLowerCase();
-						return filter.extensions.indexOf(ext) > -1;
-					}.bind(this));
-				}
-
-				var indexChanged = false;
-				for (var i = 0; i < files.length; ++i) {
-					var file = files[i];
-					if (!this._infoIndex[file]) {
-						indexChanged = true;
-						this._infoIndex[file] = {};
-					}
-				}
-				if (indexChanged) {
-					var indexFile = this._path.join(this._dir, ".mediaIndex.json");
-					this._fs.writeFile(indexFile, JSON.stringify(this._infoIndex), { encoding: "utf8" }, function(writeError) {
-						if (error) {
-							reject(writeError);
-						} else {
-							resolve(this._infoIndex);
-						}
-					}.bind(this));
-				} else {
-					resolve(this._infoIndex);
-				}
-			}
-		}.bind(this));
-	}.bind(this));
-};
-
-VideoServer.prototype.list = function() {
-	return new Promise(function(resolve) {
-		resolve(this._infoIndex);
-	}.bind(this));
-};
 
 VideoServer.prototype.serve = function() {
 	this._readInfoIndex();
@@ -125,25 +51,6 @@ VideoServer.prototype.serve = function() {
 		console.log("Server is listening on port " + this._httpPort);
 		this._http.createServer(this._serveRequest.bind(this)).listen(this._httpPort);
 	}.bind(this));
-};
-
-
-VideoServer.prototype.port = function(port) {
-	if (port === undefined) {
-		return this._httpPort;
-	} else {
-		this._httpPort = port;
-		return this;
-	}
-};
-
-VideoServer.prototype.filter = function(filter) {
-	if (filter === undefined) {
-		return this._filter;
-	} else {
-		this._filter = filter;
-		return this;
-	}
 };
 
 
@@ -212,16 +119,138 @@ VideoServer.prototype._serveRequest = function(request, response) {
 	}
 };
 
+
+VideoServer.prototype.refresh = function() {
+	this._readInfoIndex();
+};
+
+
+VideoServer.prototype.infoDefaults = function(info) {
+	if (info === undefined) {
+		// Getter
+		return this._infoDefault;
+	} else {
+		// Setter
+		this._infoDefault = info;
+		return this;
+	}
+};
+
+
+VideoServer.prototype._readInfoIndex = function() {
+	var indexFile = this._path.join(this._dir, ".mediaIndex.json");
+	try {
+		this._infoIndex = JSON.parse(this._fs.readFileSync(indexFile, { encoding: "utf8" }));
+	} catch (error) {
+		if (error.code === "ENOENT") {
+			this._fs.writeFileSync(indexFile, "{}", { encoding: "utf8" });
+		} else {
+			console.error("Error reading index: " + JSON.stringify(error));
+		}
+		this._infoIndex = {};
+	}
+};
+
+
+VideoServer.prototype._updateInfoIndex = function() {
+	return new Promise(function(resolve, reject) {
+		var directory = this._dir;
+		this._fs.readdir(directory, function(error, files) {
+			if (error) {
+				reject(error);
+			} else {
+				var filter = this.filter();
+				if (filter && filter.extensions) {
+					files = files.filter(function(file) {
+						var ext = this._path.extname(file).toLowerCase();
+						return filter.extensions.indexOf(ext) > -1;
+					}.bind(this));
+				}
+
+				var indexChanged = false;
+				for (var i = 0; i < files.length; ++i) {
+					var file = files[i];
+					if (!this._infoIndex[file]) {
+						indexChanged = true;
+						this._infoIndex[file] = {};
+					}
+				}
+				if (indexChanged) {
+					var indexFile = this._path.join(this._dir, ".mediaIndex.json");
+					this._fs.writeFile(indexFile, JSON.stringify(this._infoIndex), { encoding: "utf8" }, function(writeError) {
+						if (error) {
+							reject(writeError);
+						} else {
+							resolve(this._infoIndex);
+						}
+					}.bind(this));
+				} else {
+					resolve(this._infoIndex);
+				}
+			}
+		}.bind(this));
+	}.bind(this));
+};
+
+
+VideoServer.prototype.list = function() {
+	return new Promise(function(resolve) {
+		resolve(this._infoIndex);
+	}.bind(this));
+};
+
+
+VideoServer.prototype.port = function(port) {
+	if (port === undefined) {
+		return this._httpPort;
+	} else {
+		this._httpPort = port;
+		return this;
+	}
+};
+
+
+VideoServer.prototype.filter = function(filter) {
+	if (filter === undefined) {
+		return this._filter;
+	} else {
+		this._filter = filter;
+		return this;
+	}
+};
+
+VideoServer.prototype.tag = function(filename, requestData) {
+	// TODO: implement getter semantics
+	return new Promise(function(resolve, reject) {
+		if (requestData.tag === undefined) {
+			reject(new Error("No tag given"));
+		} else if (!this._infoIndex[filename]) {
+			reject(new Error("File not found"));
+		} else {
+			if (!Array.isArray(this._infoIndex[filename].tags)) {
+				this._infoIndex[filename].tags = [ requestData.tag ];
+			} else {
+				var pos = this._infoIndex[filename].tags.indexOf(requestData.tag);
+				if (pos === -1) {
+					this._infoIndex[filename].tags.push(requestData.tag);
+				} else {
+					this._infoIndex[filename].tags.splice(pos, 1);
+				}
+			}
+			this._writeIndex().then(resolve, reject);
+		}
+	});
+}
+
 VideoServer.prototype.rate = function(filename, requestData) {
+	// TODO: rename to rating and implement getter semantics
 	return new Promise(function(resolve, reject) {
 		if (requestData.rating === undefined) {
 			reject(new Error("No rating given"));
 		} else if (!this._infoIndex[filename]) {
 			reject(new Error("File not found"));
 		} else {
-			console.log("Rating...");
 			if (!this._infoIndex[filename].rating) {
-				console.log("Setting to... " + requestData.rating);
 				this._infoIndex[filename].rating = requestData.rating;
 				this._infoIndex[filename].numRatings = 1;
 			} else {
@@ -229,23 +258,29 @@ VideoServer.prototype.rate = function(filename, requestData) {
 				this._infoIndex[filename].rating =
 					(requestData.rating + (this._infoIndex[filename].rating * this._infoIndex[filename].numRatings)) /
 					(++this._infoIndex[filename].numRatings);
-				console.log("Setting to... " + this._infoIndex[filename].rating + " / " + this._infoIndex[filename].numRatings);
 			}
-
-			var indexFile = this._path.join(this._dir, ".mediaIndex.json");
-			this._fs.writeFile(indexFile, JSON.stringify(this._infoIndex, null, 4), { encoding: "utf8" }, function(error) {
-				console.log("Writing index file...");
-				if (error) {
-					reject(error);
-				} else {
-					resolve();
-				}
-			});
+			
+			this._writeIndex().then(resolve, reject);
 		}
 	}.bind(this));
 };
 
+VideoServer.prototype._writeIndex = function() {
+	return new Promise(function(resolve, reject) {
+		var indexFile = this._path.join(this._dir, ".mediaIndex.json");
+		this._fs.writeFile(indexFile, JSON.stringify(this._infoIndex, null, 4), { encoding: "utf8" }, function(error) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve();
+			}
+		});
+	});
+};
+
+
 VideoServer.prototype.info = function(filename) {
+	// TODO: Implement setter semantics
 	// Security checks
 	if (filename.indexOf("\0") !== -1) {
 		console.error("Hacking attempt detected");
@@ -263,6 +298,8 @@ VideoServer.prototype.info = function(filename) {
 	}
 	return {};
 };
+
+
 
 module.exports = VideoServer;
 
